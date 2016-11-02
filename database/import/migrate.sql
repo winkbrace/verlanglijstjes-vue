950d299e9127e46a9d6369bb02d7bee8e233ca3d
Bas de Ruiter <deruiterb@gmail.com>
2016-11-02 17:02:43 +0100

    Add sql to import from old database

---
+++ database/import/migrate.sql
@@ -0,0 +1,17 @@
+truncate table wish_lists;
+
+insert into wish_lists (user_id, description, link, claimed_by, claimed_at, created_at, updated_at, deleted_at)
+select u.id user_id
+,      v.omschrijving description
+,      ifnull(v.link, v.plaatje) link
+,      uc.id claimed_by
+,      v.claimed_date claimed_at
+,      v.create_date created_at
+,      v.change_date updated_at
+,      case when v.deleted = 1 or v.gekregen = 1 then v.change_date else null end deleted_at
+from   verlanglijstjes v
+join   familie f on v.fam_id = f.id
+join   users u on f.naam = u.name
+left join familie fc on v.claimed_by = fc.id
+left join users uc on fc.naam = uc.name
+order by created_at;
